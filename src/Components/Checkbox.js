c00f5a280d158d58747ea71398ac083cab48e5c7
emilie <emi.m.humbert@googlemail.com>
2020-08-18 16:48:18 +0100

    fix checkbox onChange function

--- src/Components/Checkbox.js
+++ src/Components/Checkbox.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { Checkbox as CheckboxAnt } from "antd";
 
-const Checkbox = ({ disabled, todo, handleCheck }) => {
-  return (
-    <CheckboxAnt disabled={disabled} onChange={(todo) => handleCheck(todo)} />
-  );
+const Checkbox = ({ disabled, handleCheck }) => {
+  return <CheckboxAnt disabled={disabled} onChange={handleCheck} />;
 };
 
 export default Checkbox;
